17b3fb71a6e5045f44328f96623bbb26515bcd92
Rakesh <dr8452799@gmail.com>
2025-07-01 11:56:38 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -34,18 +34,7 @@
 
 
 
--- JOINS
---List employee names along with their department names (even if no department).
-   SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-LEFT JOIN departments d ON e.Dept_id = d.Dept_id;
 
---Inner JOIN
--- List all employees and their department names (only if they belong to a department).
-   -- Inner Join (Returns matching rows from both tables.)
-SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-INNER JOIN departments d ON e.Dept_id = d.Dept_id;
 
 -- LEFT JOIN (LEFT OUTER JOIN)
 -- Returns all rows from the left table, plus matched rows from the right table. Unmatched rows get NULL.
