cdbc55bd9e39f757cb81765fe5ee46d300411ad9
Rakesh <dr8452799@gmail.com>
2025-07-01 12:05:03 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,18 +1,6 @@
 -- Hacker Rank SQL Queries
 --Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
   select count(city) - count(distinct city) from station;
---Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates. 
-  SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiou]';
---Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
-  select DISTINCT CITY from STATION where CITY RLIKE '[aeiouAEIOU]$';
---Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
-  select CITY from STATION where CITY REGEXP '^[aeiouAEIOU]' AND CITY REGEXP '[aeiouAEIOU]$';
---Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
-  select DISTINCT CITY from STATION where CITY not REGEXP '^[aeiouAEIOU]';
---Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
-  select DISTINCT CITY from STATION where CITY not REGEXP '[aeiouAEIOU]$';
---Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
-  select DISTINCT CITY from STATION where CITY NOT  REGEXP '^[aeiouAEIOU]' OR CITY NOT REGEXP '[aeiouAEIOU]$';
 --Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  
 --per month who have been employees for less than  months. Sort your result by ascending employee_id.
   select name from Employee where salary>2000 and months<10 order by employee_id asc;
