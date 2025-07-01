7d364c9886b2a3a7e84726a5ee6cd922257aa6d6
Rakesh <dr8452799@gmail.com>
2025-07-01 12:02:24 +0530

    Update Aggregations.sql

--- Aggregations.sql
+++ Aggregations.sql
@@ -1,1 +1,17 @@
+    --Aggregation Functions Hacker Rank
+--Query the total population of all cities in CITY where District is California.
+  select sum(population) from CITY where DISTRICT='California';
+--Query a count of the number of cities in CITY having a Population larger than .
+  select count(NAME) from CITY where Population>100000;
+--Query the average population of all cities in CITY where District is California.
+  select avg(population) from CITY where DISTRICT='California';
+--Query the average population for all cities in CITY, rounded down to the nearest integer.
+  select ROUND(AVG(population)) from city;
+--Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+  select sum(population) from city where countrycode='JPN';
+--Query the difference between the maximum and minimum populations in CITY.
+  select max(population)-min(population) from CITY;
+--Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to  decimal places.
+  select TRUNCATE(sum(LAT_N),4) from STATION where LAT_N>38.7880 AND LAT_N<137.2345;
+
 
