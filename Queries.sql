d9e6ac39d4af25611a97841715d8aaac6ccc2342
Rakesh <dr8452799@gmail.com>
2025-07-01 11:57:50 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -33,37 +33,6 @@
 
 
 
-
-
-
--- LEFT JOIN (LEFT OUTER JOIN)
--- Returns all rows from the left table, plus matched rows from the right table. Unmatched rows get NULL.
---  Use case: Show all employees, even those not assigned to a department.
-   SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-LEFT JOIN departments d ON e.Dept_id = d.Dept_id;
-
--- RIGHT JOIN (RIGHT OUTER JOIN)
--- Returns all rows from the right table, plus matched rows from the left table
---Use case: List all departments, including those with no employees.
-SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-RIGHT JOIN departments d ON e.Dept_id = d.Dept_id;
-
---FULL OUTER JOIN (Not supported in MySQL)
---Returns all records when there is a match in one of the tables.
---Use case: List all employees and all departments, showing NULLs where there’s no match.
--- MySQL workaround using UNION:
-SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-LEFT JOIN departments d ON e.Dept_id = d.Dept_id
-
-UNION
-
-SELECT e.Emp_Name, d.Dept_Name
-FROM employees e
-RIGHT JOIN departments d ON e.Dept_id = d.Dept_id;
-
 --SELF JOIN
 --A table joined with itself.
 --Use case: Show employees and their managers using a Manager_id foreign key.
