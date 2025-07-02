1bc6e8568279c9cb6fdde25dfb885396e6639e3f
Rakesh <dr8452799@gmail.com>
2025-07-02 14:21:21 +0530

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -1,4 +1,14 @@
 -- JOINS
+     -- Defination: Joins in Sql used to Comnine Two or More Tables Based on releated Column Them.
+-- Types Of Joins
+  -- Inner Join : is used to combine rows from two or more tables based on matching condition between them.It return Both Records where Specific Conditon is True.
+  -- Right Join : is Used to return all Records from right table as well as matching records from the left table. If no matching records will be found in left table , return null values.
+  -- Left Join : is used to return all Records from left table as well as matching records from the right table. If no matching records will be found in right table, return null values.
+  -- Full Join : Combines the results of both Left Join and Right Join. it return all records both tables. 
+                -- In Mysql work Bench full join is not working. We've to use Union operator in Btw Left JOin, Right Join.
+  -- Self Join : is a join where a table is joined with itself to compare rows within the same table.
+  -- Cross Join : is used to return the Cartersian Product of two tables, meaning it combines every row from the first table with every row from the second table. It doesn't require any condition.
+
 --List employee names along with their department names (even if no department).
    SELECT e.Emp_Name, d.Dept_Name
 FROM employees e
@@ -36,22 +46,18 @@ UNION
 SELECT e.Emp_Name, d.Dept_Name
 FROM employees e
 RIGHT JOIN departments d ON e.Dept_id = d.Dept_id;
-
 --SELF JOIN
 --A table joined with itself.
 --Use case: Show employees and their managers using a Manager_id foreign key.
 SELECT e1.Emp_Name AS Employee, e2.Emp_Name AS Manager
 FROM employees e1
 JOIN employees e2 ON e1.Manager_id = e2.Emp_id;
-
-
 --CROSS JOIN
 --Returns Cartesian product (every row in A joins with every row in B).
 -- Use case: Generate all possible employee-department pairings (e.g., for simulation/testing).
 SELECT e.Emp_Name, d.Dept_Name
 FROM employees e
 CROSS JOIN departments d;
-
 
 -- JOINS QUERIES FROM HACKER RANK
 --Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
